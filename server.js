5b3b3bb34e2c6e109bc573bd0cacd8ea8bc335a4
Julius <julius@uusinarkaus.com>
2016-10-08 00:18:49 +0300

    server URL try

--- server.js
+++ server.js
@@ -14,7 +14,7 @@
 
     // configuration =================
 
-    var url = 'mongodb://servu:' + process.env.SALASANA + '@ds053206.mlab.com:53206/asblaulut';
+    var url = 'mongodb://servu:nakkivene@ds053206.mlab.com:53206/asblaulut';
 
     mongoose.connect(url);
 
